706b634183df51f4212b5d03faa68dcdf75d08df
unknown <anna.hurieva79@gmail.com>
2023-02-18 00:29:45 +0100

    end2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите элементы массива через пробел: ");
+string num = Console.ReadLine()!;
+string[] numS = num.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+int count = 0;
+foreach (string el in numS)
+{
+    if (el.Length <= 3)
+        count++;
+}
+
+int i = 0;
+string[] Ar = new string[count];
+foreach (string el in numS)
+{
+    if (el.Length <= 3)
+    {
+        Ar[i] = el;
+        i++;
+    }
+}
+Console.Write($"[{String.Join(' ', Ar)}]");
